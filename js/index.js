54ff5f999b9fe891629907dabb4c71966e909b89
Atul Rauthan <atulrauthan7@gmail.com>
2021-03-03 12:48:55 +0530

    added imagese and tweets

--- js/index.js
+++ js/index.js
@@ -1,10 +1,23 @@
 const URL = "http://localhost:3000/tweets";
 
+const onEnter = (e) => {
+    if(e.key == "Enter") {
+        getTwitterData()
+    }
+}
+ 
 /**
  * Retrive Twitter Data from API
  */
 const getTwitterData = () => {
-
+    const user_search_input = document.getElementById('user-search-input').value
+    if(!user_search_input) return
+    const encoded_query = encodeURIComponent(user_search_input)
+    fetch(`${URL}?q=${encoded_query}&count=10`).then((response) => {
+        return response.json()
+    }).then((data) => {
+        buildTweets(data.statuses, 1)
+    })
 }
 
 /**
@@ -29,14 +42,47 @@ const nextPageButtonVisibility = (metadata) => {
  * Build Tweets HTML based on Data from API
  */
 const buildTweets = (tweets, nextPage) => {
+    let tweetsContent = ""
+    tweets.map((tweet) => {
+        tweetsContent += `
+        <div class="tweet-container">
+            <div class="tweet-user-info">
+                <div class="tweet-user-profile" style="background-image:url('${tweet['user']['profile_image_url']}')">
+                    
+                </div>
+                <div class="tweet-user-name-container">
+                    <div class="tweet-user-fullname">${tweet['user']['name']}</div>
+                    <div class="tweet-user-username">${tweet['user']['screen_name']}</div>
+                </div>
+            </div>
+        `
+        if(tweet.entities.media) {
+            tweetsContent += buildImages(tweet.entities.media)
+        }
 
+        tweetsContent += `<div class="tweet-text-container">
+                ${tweet['full_text']}
+            </div>
+            <div class="tweet-date-container">
+                20 hours ago
+            </div>
+        </div>
+        `
+    })
+    document.querySelector('.tweets-list').innerHTML += tweetsContent
 }
 
 /**
  * Build HTML for Tweets Images
  */
 const buildImages = (mediaList) => {
-
+    let tweetsContent = ""
+    tweetsContent += `<div class="tweet-media-container">`
+        for(image in mediaList) {
+            tweetsContent += `<div class="tweet-image" style="background-image:url('${mediaList[0]['media_url']}')"></div>`
+        }
+    tweetsContent += `</div>`
+    return tweetsContent
 }
 
 /**
